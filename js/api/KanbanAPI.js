775914e751f5243272cfb590c8ad6f377589c8b8
Shizuco <93375450+Shizuco@users.noreply.github.com>
2021-11-01 13:51:17 +0300

    Add files via upload

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,135 @@
+export default class KanbanAPI {
+
+	static insertColumn() {
+		const data = read();
+		const column = {
+			id: Math.floor(Math.random() * 100000),
+			title: "New column"
+		};
+		data.push({id: column.id, items: []})
+		save(data);
+
+		return column;
+	}
+
+	static getColumn(columnId){
+		const column = readc().find(column=>column.id==columnId);
+		return column;
+	}
+
+	static getItems(columnId) {
+		const column = read().find(column => column.id == columnId);
+
+		if (!column) {
+			return [];
+		}
+
+		return column.items;
+	}
+
+	static insertItem(columnId, content) {
+		const data = read();
+		const column = data.find(column => column.id == columnId);
+		const item = {
+			id: Math.floor(Math.random() * 100000),
+			content
+		};
+
+		if (!column) {
+			throw new Error("Column does not exist.");
+		}
+
+		column.items.push(item);
+		save(data);
+
+		return item;
+	}
+
+	static updateItem(itemId, newProps) {
+		const data = read();
+		const [item, currentColumn] = (() => {
+			for (const column of data) {
+				const item = column.items.find(item => item.id == itemId);
+
+				if (item) {
+					return [item, column];
+				}
+			}
+		})();
+
+		if (!item) {
+			throw new Error("Item not found.");
+		}
+
+		item.content = newProps.content === undefined ? item.content : newProps.content;
+
+		// Update column and position
+		if (
+			newProps.columnId !== undefined
+			&& newProps.position !== undefined
+		) {
+			const targetColumn = data.find(column => column.id == newProps.columnId);
+
+			if (!targetColumn) {
+				throw new Error("Target column not found.");
+			}
+
+			// Delete the item from it's current column
+			currentColumn.items.splice(currentColumn.items.indexOf(item), 1);
+
+			// Move item into it's new column and position
+			targetColumn.items.splice(newProps.position, 0, item);
+		}
+
+		save(data);
+	}
+
+	static deleteItem(itemId) {
+		const data = read();
+
+		for (const column of data) {
+			const item = column.items.find(item => item.id == itemId);
+
+			if (item) {
+				column.items.splice(column.items.indexOf(item), 1);
+			}
+		}
+
+		save(data);
+	}
+}
+
+function read() {
+	const json = localStorage.getItem("kanban-data");
+
+	if (!json) {
+		return [
+			{
+				id: 1,
+				items: []
+			}
+		];
+	}
+
+	return JSON.parse(json);
+}
+
+
+function readc() {
+	const json = localStorage.getItem("kanban-data");
+
+	if (!json) {
+		return [
+			{
+				id: 1,
+				title: "New"
+			}
+		];
+	}
+
+	return JSON.parse(json);
+}
+
+function save(data) {
+	localStorage.setItem("kanban-data", JSON.stringify(data));
+}
